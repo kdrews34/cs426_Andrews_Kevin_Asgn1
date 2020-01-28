3ac132fb8cae46a06383172eea47faa42aa2c955
Kevin Andrews <kdrews34@gmail.com>
2020-01-27 20:11:32 -0600

    added Assets directory

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public int maxScore = 5;
+ 
+    int score;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+   
+    public void AddPoint()
+    {
+        score++;
+        scoreText.text = "Score: " + score;
+    }
+}
